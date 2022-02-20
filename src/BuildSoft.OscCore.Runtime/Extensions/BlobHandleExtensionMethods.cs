873929830d05536bc1dd483a95190eb17eab229c
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-02-20 21:47:22 +0900

    update: Add extension methods for BlobHandle or BlobString.

---
+++ src/BuildSoft.OscCore.Runtime/Extensions/BlobHandleExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlobHandles;
+
+/// <summary>
+/// Extension methods for <see cref="BlobHandle"/> or <see cref="BlobString"/>.
+/// </summary>
+public static class BlobHandleExtensionMethods
+{
+    public static unsafe ReadOnlySpan<byte> AsSpan(this BlobHandle blobHandle) => new(blobHandle.Pointer, blobHandle.Length);
+    public static unsafe ReadOnlySpan<byte> AsSpan(this BlobString blobString) => blobString.Handle.AsSpan();
+}
