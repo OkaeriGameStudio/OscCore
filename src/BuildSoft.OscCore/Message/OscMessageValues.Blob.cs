db52380950385a45957dcc4de590ad90c203a1e0
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-02-23 23:18:59 +0900

    Update: Implemented `OscMessageValues.ReadBlobElement(int)`.

--- src/BuildSoft.OscCore/Message/OscMessageValues.Blob.cs
+++ src/BuildSoft.OscCore/Message/OscMessageValues.Blob.cs
@@ -12,6 +12,32 @@ public sealed unsafe partial class OscMessageValues
     /// Checks the element type before reading, and throw <see cref="InvalidOperationException"/> if the element is not a blob.
     /// </summary>
     /// <param name="index">The element index</param>
+    /// <returns>The array copied blob contents into</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public byte[] ReadBlobElement(int index)
+    {
+#if OSCCORE_SAFETY_CHECKS
+        if (OutOfBounds(index)) return default;
+#endif
+        switch (_tags[index])
+        {
+            case TypeTag.Blob:
+                var offset = _offsets[index];
+                var size = ReadIntIndex(offset);
+                var dataStart = offset + 4;    // skip the size int
+                var copyTo = new byte[size];
+                Buffer.BlockCopy(_sharedBuffer, dataStart, copyTo, 0, size);
+                return copyTo;
+            default:
+                throw new InvalidOperationException();
+        }
+    }
+
+    /// <summary>
+    /// Read a blob element.
+    /// Checks the element type before reading, and throw <see cref="InvalidOperationException"/> if the element is not a blob.
+    /// </summary>
+    /// <param name="index">The element index</param>
     /// <param name="copyTo">
     /// The array to copy blob contents into.
     /// Will be resized if it lacks sufficient capacity
