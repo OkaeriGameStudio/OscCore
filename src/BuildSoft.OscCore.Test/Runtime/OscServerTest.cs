7552fc9d5ccb5e0d20b5aac8211c5c393845e475
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-02-24 02:17:49 +0900

    Test: Add test for ff61988.

---
+++ src/BuildSoft.OscCore.Test/Runtime/OscServerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BuildSoft.OscCore.Test.Runtime;
+
+[TestOf(typeof(OscServer))]
+public class OscServerTest
+{
+    private OscServer _server;
+
+    [SetUp]
+    public void Setup()
+    {
+        _server = new OscServer(7000);
+    }
+
+    [Test]
+    public void CallbackTest()
+    {
+        MonitorCallback callback1 = (_, _) => { };
+        MonitorCallback callback2 = (_, _) => { };
+
+        _server.AddMonitorCallback(callback1);
+        _server.AddMonitorCallback(callback1);
+
+        Assert.IsTrue(_server.RemoveMonitorCallback(callback1));
+        Assert.IsFalse(_server.RemoveMonitorCallback(callback2));
+        Assert.IsTrue(_server.RemoveMonitorCallback(callback1));
+        Assert.IsFalse(_server.RemoveMonitorCallback(callback1));
+    }
+}
