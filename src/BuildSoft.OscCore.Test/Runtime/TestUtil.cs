18f28df1985040fa4ac4e1aa0a212243c1ce3060
ChanyaKushima <63221603+ChanyaVRC@users.noreply.github.com>
2022-12-10 12:16:55 +0900

    Made name simply.

--- src/BuildSoft.OscCore.Test/Runtime/TestUtil.cs
+++ src/BuildSoft.OscCore.Test/Runtime/TestUtil.cs
@@ -57,7 +57,7 @@ public static class TestUtil
         var bytes = new byte[byteCount];
         for (int i = 0; i < bytes.Length; i += 4)
         {
-            var f = TestUtil.SharedRandom.Next() * 2f - 1f;
+            var f = SharedRandom.Next() * 2f - 1f;
             var fBytes = BitConverter.GetBytes(f);
             for (int j = 0; j < fBytes.Length; j++)
             {
@@ -73,7 +73,7 @@ public static class TestUtil
         var bytes = new byte[byteCount];
         for (int i = 0; i < bytes.Length; i += 4)
         {
-            var iValue = TestUtil.SharedRandom.Next(-1000, 1000);
+            var iValue = SharedRandom.Next(-1000, 1000);
             var iBytes = BitConverter.GetBytes(iValue);
             for (int j = 0; j < iBytes.Length; j++)
                 bytes[i + j] = iBytes[j];
@@ -87,7 +87,7 @@ public static class TestUtil
         var bytes = new byte[byteCount];
         for (int i = 0; i < bytes.Length; i += 4)
         {
-            var iValue = TestUtil.SharedRandom.Next(0, 255);
+            var iValue = SharedRandom.Next(0, 255);
             var iBytes = BitConverter.GetBytes(iValue);
             for (int j = 0; j < iBytes.Length; j++)
                 bytes[i + j] = iBytes[j];
@@ -101,10 +101,10 @@ public static class TestUtil
         var bytes = new byte[byteCount];
         for (int i = 0; i < bytes.Length; i += 4)
         {
-            var port = (byte)TestUtil.SharedRandom.Next(1, 16);
-            var status = (byte)TestUtil.SharedRandom.Next(0, 127);
-            var data1 = (byte)TestUtil.SharedRandom.Next(10, 255);
-            var data2 = (byte)TestUtil.SharedRandom.Next(10, 255);
+            var port = (byte)SharedRandom.Next(1, 16);
+            var status = (byte)SharedRandom.Next(0, 127);
+            var data1 = (byte)SharedRandom.Next(10, 255);
+            var data2 = (byte)SharedRandom.Next(10, 255);
             bytes[i] = port;
             bytes[i + 1] = status;
             bytes[i + 2] = data1;
@@ -119,12 +119,12 @@ public static class TestUtil
         var bytes = new byte[count];
         for (int i = 0; i < bytes.Length; i += 8)
         {
-            var seconds = TestUtil.SharedRandom.Next(0, 255);
+            var seconds = SharedRandom.Next(0, 255);
             var sBytes = BitConverter.GetBytes(seconds);
             for (int j = 0; j < sBytes.Length; j++)
                 bytes[i + j] = sBytes[j];
 
-            var fractions = TestUtil.SharedRandom.Next(0, 10000000);
+            var fractions = SharedRandom.Next(0, 10000000);
             var fBytes = BitConverter.GetBytes(fractions);
 
             var end = 4 + fBytes.Length;
